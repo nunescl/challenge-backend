b548e5e38a016d99d2baece3b2838ffd16732808
Ana Clara <anaclaranunes@gmail.com>
2022-06-15 20:35:18 -0300

    make it better

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,11 @@ const PORT = env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: "GET, POST, PUT, DELETE"
+}
+))
 
 app.get('/', (req: Request, res: Response)=>{res.json({status: 'sucess', version:'1.0.0'}).status(200)});
 app.use('/categories', CategoryRoutes);
