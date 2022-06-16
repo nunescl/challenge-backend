949cf689ef2c7c71040069a32d5c7558c269af34
Ana Clara <anaclaranunes@gmail.com>
2022-06-16 11:42:21 -0300

    make it better

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,11 @@ const PORT = env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use(cors({
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+// app.options(cors({
 //   origin: '*',
 //   methods: "GET, POST, PUT, DELETE"
 // }
