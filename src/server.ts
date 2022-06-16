039fac7b220ac0fe88bb503abbf9d51f8a5e69c6
Ana Clara <anaclaranunes@gmail.com>
2022-06-15 21:24:50 -0300

    make it better

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,17 @@ import { env } from "./config/environment-variables";
 import { errorHandler } from "./middlewares";
 import CategoryRoutes from './routes/router-category'
 import ProductRoutes from './routes/router-product'
-import cors from 'cors'
+// import cors from 'cors'
 
 const PORT = env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-  origin: '*',
-  methods: "GET, POST, PUT, DELETE"
-}
-))
+// app.use(cors({
+//   origin: '*',
+//   methods: "GET, POST, PUT, DELETE"
+// }
+// ))
 
 app.get('/', (req: Request, res: Response)=>{res.json({status: 'sucess', version:'1.0.0'}).status(200)});
 app.use('/categories', CategoryRoutes);
