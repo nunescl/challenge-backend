e1a0bb34caea84d7d21aebb5dc12d907632da92d
Ana Clara <anaclaranunes@gmail.com>
2022-06-19 18:51:09 -0300

    cors config

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,10 @@ fs.mkdirSync(directory)
 
 const PORT = env.PORT || 3000
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE']
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
